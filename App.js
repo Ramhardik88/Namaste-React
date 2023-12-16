d4b17dbc0db8bd26f658c94953929500a7b7d1a2
Ramkumar S <ramkumar@Ramkumars-MacBook-Pro.local>
2023-12-16 16:09:48 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "heading" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "Hello from H1 React"),
